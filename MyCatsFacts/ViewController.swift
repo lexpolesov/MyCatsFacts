9781ba5c7b178da752b5cacdd967ea4b4348d7f8
Admin <admin@mac-admin.local>
2018-12-15 20:01:37 +0400

    Initial Commit

---
+++ MyCatsFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCatsFacts
+//
+//  Created by Admin on 15.12.2018.
+//  Copyright © 2018 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
